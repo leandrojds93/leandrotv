43a610ae6b1148df8f4691c8366ec3a53b4e0e44
leandrojds93 <leandro.junio.duarte@gmail.com>
2025-03-18 23:09:23 -0300

    update logos.vue

--- components/landing/Logos.vue
+++ components/landing/Logos.vue
@@ -4,12 +4,12 @@
     <div
       class="flex gap-8 md:gap-20 items-center justify-center mt-10 flex-wrap"
     >
-      <p>TVBOX</p>
-      <p>SAMSUNG TIZEN</p>
-      <p>LG WEB OS</p>
-      <p>TV ANDROID</p>
-      <p>ROKU TV</p>
-      <p>WINDOWS & MAC</p>
+      <p>• TVBOX</p>
+      <p>• SAMSUNG TIZEN</p>
+      <p>• LG WEB OS</p>
+      <p>• TV ANDROID</p>
+      <p>• ROKU TV</p>
+      <p>• WINDOWS & MAC</p>
     </div>
   </div>
 </template>
